d53669507f8574319ac47dc7b7d1d26d91e9e3e0
libertyernie <maybeway36@gmail.com>
2023-11-19 16:14:32 -0600

    Re-enable nunchuk control for build without Xbox controller support

--- common/include/NintendontVersion.h
+++ common/include/NintendontVersion.h
@@ -14,7 +14,6 @@
 // This should only be set in custom builds, i.e. not mainline.
 #define NIN_SPECIAL_VERSION			"-libertyernie"
 
-#define LI_NONUNCHUK
 #define LI_GAMEPADASCCPRO
 #define LI_NOSWAP
 #define LI_NOEXIT
